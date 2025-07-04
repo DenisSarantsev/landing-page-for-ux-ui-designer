f5e655ff0563f149d1d1cd5ea948f75287ca5fed
Denis <sarantsev.denis.94@gmail.com>
2025-07-04 03:12:13 +0300

    Налаштував сладер для мобільних пристроїв

--- src/script/modals.ts
+++ src/script/modals.ts
@@ -1,18 +1,39 @@
-/*
-// Получаем обе модалки
+document.addEventListener("DOMContentLoaded", () => {
+// Получаем модалку
 const projectModal = document.querySelector<HTMLElement>(".project-modal");
-// Показывать ли модалку
-let showProjectModalStatus: boolean
-// ID текущего проекта
+// Показывать ли модалку (false - не показывать, "project" / "workplace" - показывать)
+let showProjectModalStatus: boolean | string;
+// ID текущего проекта / воркплейса
 let currentModalProject: number;
+// Кнопка закрытия модалки
+const modalCloseButton = document.querySelector<HTMLElement>(".modal-close-button");
+modalCloseButton?.addEventListener("click", (event) => {
+	
+})
 
 // Получаем все карточки проектов на странице
 const allProjectCards = document.querySelectorAll<HTMLElement>(".work-card");
 if ( allProjectCards && [...allProjectCards].length > 0 ) {
 	[...allProjectCards].forEach((card) => {
 		card.addEventListener("click", (event) => {
-			event.
+			showProjectModalStatus = "project"
 		})
 	})
 }
-*/
+// Получаем все карточки воркплейсов на странице (десктоп и мобайл)
+const allWorkplaceCards = document.querySelectorAll<HTMLElement>(".work-history-card")
+if ( allWorkplaceCards && [...allWorkplaceCards].length > 0 ) {
+	[...allWorkplaceCards].forEach((card) => {
+		card.addEventListener("click", (event) => {
+			showProjectModalStatus = "workplace"
+		})
+	})
+}
+
+
+
+
+
+
+
+})
--- src/script/swiper.ts
+++ src/script/swiper.ts
@@ -6,65 +6,83 @@ import { loadWorkplaces } from './common';
 
 // Получаем данные из json
 const swiperWorkplaces = await loadWorkplaces();
-
-const swiper = new Swiper('.swiper', {
-	slidesPerView: 2.2, // Показывать 3 слайда одновременно
-  spaceBetween: 20,
-
-	// Адаптивные настройки
-	// breakpoints: {
-	// 	280: {
-	// 		slidesPerView: 2.05,
-	// 		spaceBetween: 20
-	// 	},
-	// 	600: {
-	// 		slidesPerView: 2,
-	// 		spaceBetween: 20
-	// 	},
-	// }
-});
-
+// Получаем контейнер для слайдов
 const slidesWrapper = document.querySelector<HTMLElement>(".swiper-wrapper");
-const allSlides = document.querySelectorAll(".swiper-slide");
+// Вставляем слайды в контейнер
+if ( swiperWorkplaces && swiperWorkplaces.length > 0 )
+swiperWorkplaces.forEach((slide) => {
+	slidesWrapper?.insertAdjacentHTML("beforeend", `
+		<div class="swiper-slide">
+			<div data-workplace-swiper-slide-id=${slide.id} class="work-history-card">
+				<div class="work-history-card__wrapper">
+					<div class="work-history-card__top">
+						<h3 class="work-history-card__title">${slide.post}</h3>
+						<div class="work-history-card__subtitle">${slide.formate}</div>
+					</div>
+					<div class="work-history-card__info">
+						<img src="/img/workplace/${slide.id}/company-logo.png" alt="company logo" class="work-history-card__info_image">
+						<div class="work-history-card__info_data">
+							<div class="work-history-card__info_company-name">${slide.company}</div>
+							<div class="work-history-card__info_address">${slide['company-address']}</div>
+						</div>
+					</div>
+					<div class="work-history-card__period">
+						<div class="work-history-card__period_title">From</div>
+						<div class="work-history-card__period_subtitle">${slide.period.from}</div>
+					</div>
+					<div class="work-history-card__period">
+						<div class="work-history-card__period_title">To</div>
+						<div class="work-history-card__period_subtitle">${slide.period.to}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	`)
+})
+console.log("1")
 
-console.log(allSlides)
 
-if (slidesWrapper) {
-	// Середина контейнера со слайдерами
-	const centerOfWrapper = slidesWrapper?.offsetWidth / 2;
-}
+const initSwiper = async () => {
+	console.log("2")
+	try {
+		const swiper = new Swiper('.swiper', {
+			slidesPerView: 'auto', // Показывать 3 слайда одновременно
+			spaceBetween: 20,
 
-
-swiper.on('sliderMove', () => {
-  // Эта функция будет вызываться во время перетаскивания/движения слайда
-  console.log('Слайд двигается!');
-	checkCardsMove();
-});
-
-swiper.on('transitionEnd', () => {  
-	console.log('Переход завершён');
-});
-
-swiper.on('transitionStart', () => {
-	console.log('Начался переход между слайдами');
-});
-
-
-// Отслеживание расположения всех карточек и смещение
-const checkCardsMove = (): void => {
-	if ( allSlides ) {
-		// Пробегаемся по всем слайдам
-		allSlides.forEach((slide, index) => {
-			// Получаем расположение слайда
-			const rect = slide.getBoundingClientRect(); 
-			const left = rect.left; // расстояние от левого края viewport до элемента
-			const right = rect.right; // расстояние от левого края viewport до правого края элемента
-			const width = rect.width; // ширина элемента
-			// 
-			if ( index === 0 ) {
-				console.log(left)
+			// Адаптивные настройки
+			breakpoints: {
+				0: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+				},
+				360: {
+					slidesPerView: 1.2,
+					spaceBetween: 20,
+				},
+				400: {
+					slidesPerView: 1.3,
+					spaceBetween: 20,
+				},
+				450: {
+					slidesPerView: 1.4
+				},
+				500: {
+					slidesPerView: 1.5
+				},
+				600: {
+					slidesPerView: 2
+				},
+				670: {
+					slidesPerView: 2.2,
+				},
 			}
-		})
+		});		
+	}
+	catch (err) {
+		console.log(err)
 	}
 }
+// Вызываем функцию
+initSwiper();
+
 
